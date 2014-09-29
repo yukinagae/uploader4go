f0bb51b825602c9b6465e90d51712d7df9947481
yukinagae <yuki.nagae1130@gmail.com>
2014-09-29 09:18:25 +0900

    first commit

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"net/http"
+	"text/template"
+)
+
+type Page struct {
+	Title string
+}
+
+func main() {
+	fmt.Println("helo")
+
+	// Routing
+	r := mux.NewRouter()
+
+	// for "/"
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t, _ := template.ParseFiles("view.html")
+		t.Execute(w, &Page{Title: "hoge"})
+	}).Methods("GET")
+
+	n := negroni.Classic()
+	n.UseHandler(r)
+	n.Run(":3000")
+}
